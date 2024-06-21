fd75a125c310ccacd2e3bb389e9b7b9418fb887e
Marcela Costa <designer.marcela.amorim@gmail.com>
2024-06-21 06:48:54 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const hamMenu = document.querySelector(".nav__hamburguer");
+const offScreenMenu = document.querySelector(".nav__off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+});
